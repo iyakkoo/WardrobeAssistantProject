709617497126f0b8b96beafc33ca5666c4cbb629
Dominika Woźniak <dominika.dorota.wozniak@gmail.com>
2019-04-07 13:30:20 +0200

    pracy ciąg dalszy, w szafie już działa rotator na ubrania

--- js_data/WardrobeScript.js
+++ js_data/WardrobeScript.js
@@ -1,3 +1,4 @@
+/*kod do slajdu*/
 var number = Math.floor(Math.random()*3)+1;
 var timer1 = 0;
 var timer2 = 0;
@@ -28,3 +29,26 @@ function changeSlide(){
     timer1 = setTimeout("changeSlide()", 6000);
     timer2 = setTimeout("toHide()", 5500);
 }
+/*kod do działania szafy*/
+
+var picture = new Array(4);
+picture[0] = "img/zestawy/zestaw0.jpg";
+picture[1] = "img/zestawy/zestaw1.jpg";
+picture[2] = "img/zestawy/zestaw2.jpg";
+picture[3] = "img/zestawy/zestaw3.jpg";
+var i = 0;
+
+function clickRightButton(){
+    i = (i + 1)%picture.length ;
+    document.getElementById("rotator").innerHTML = '<img src="' + picture[i] + '" alt=""/>'
+}
+
+function clickLeftButton(){
+    if(i == 0) 
+        i = picture.length-1;
+    else
+        i = (i - 1)%picture.length;
+    document.getElementById("rotator").innerHTML = '<img src="' + picture[i] + '" alt=""/>'
+}
+
+
