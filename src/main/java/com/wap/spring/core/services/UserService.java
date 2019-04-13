63719caaa6b7f16c46edc8b3b407a488c21c36bd
iyakkoo <jakub.koziol.prv@gmail.com>
2019-04-14 00:09:13 +0200

    Upload 1.6 dont merge if you want your app still working :)

--- src/main/java/com/wap/spring/core/services/UserService.java
+++ src/main/java/com/wap/spring/core/services/UserService.java
@@ -1,4 +1,113 @@
 package com.wap.spring.core.services;
 
+import com.wap.spring.data.model.Item;
+import com.wap.spring.data.model.User;
+import com.wap.spring.data.repositories.ItemRepository;
+import com.wap.spring.data.repositories.UserRepository;
+import com.wap.spring.dto.ItemListDTO;
+import com.wap.spring.dto.LoggedUserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
 public class UserService {
+
+    private final ItemRepository itemRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository, ItemRepository itemRepository) {
+        this.userRepository = userRepository;
+        this.itemRepository = itemRepository;
+    }
+
+
+//    public List<ItemListDTO> getItemsForUser(LoggedUserDTO user) {
+//        // TODO Uzupełnij implementację z wykorzystaniem Spring Data
+//        return userRepository.getAllItemsForUser(user.getId())
+//                .stream()
+//                .map(this::buildItemDTO)
+//                .collect(Collectors.toList());
+//        return userRepository.findAllNonDistinctObtainedSkillsForUser(user.getId())
+//                .stream()
+//                .filter(skill -> skill != null)
+//                .map(skill -> {
+//                    SkillDTO skillDTO = new SkillDTO();
+//                    skill.setId(skill.getId());
+//                    skillDTO.setName(skill.getName());
+//                    skillDTO.setCategory(skill.getCategory());
+//                    return skillDTO;
+//                })
+//                .collect(Collectors.toList());
+//        User userEntity = userRepository.findById(user.getId()).orElse(null);
+//        if(userEntity == null) {
+//            return Collections.emptyList();
+//        }
+//
+//        List<SkillDTO> skills = new LinkedList<>();
+//        for (KnowledgeSource source: userEntity.getKnownSources()){
+//            for (Skill skillEntity : source.getConnectedSkills() ) {
+//                SkillDTO skillDTO = new SkillDTO();
+//                skillDTO.setId(skillEntity.getId());
+//                skillDTO.setName(skillEntity.getName());
+//                skillDTO.setCategory(skillEntity.getCategory());
+//                skills.add(skillDTO);
+//            }
+//        }
+//        return skills;
+//    }
+//
+//    public void addNewSource(LoggedUserDTO user, ItemListDTO source) {
+//        // TODO Uzupełnij implementację z wykorzystaniem Spring Data
+//        User userEntity = userRepository.findById(user.getId()).orElseThrow(RuntimeException::new);
+//        Item item = itemRepository.findById(source.getId()).orElseThrow(RuntimeException::new);
+//        if (!item.getActive()){
+//            throw new IllegalArgumentException("Nie można potwierdzić nieaktywnego źródła wiedzy!");
+//        }
+//        if (!userEntity.getKnownSources().contains(item)){
+//            userEntity.getKnownSources().add(item);
+//        }
+//    }
+
+    public boolean checkCredentials(String login, String password) {
+        // TODO Uzupełnij implementację z wykorzystaniem Spring Data
+        boolean existsUser = userRepository.checkIfUserExists(login, password);
+
+        return existsUser;
+    }
+//
+//    public LoggedUserDTO getUser(String login, String password) {
+//        // TODO Uzupełnij implementację z wykorzystaniem Spring Data
+//
+//        LoggedUserDTO loggedUserDTO = null;
+//        User user = userRepository.getUserByLoginAndPassword(login, password);
+//
+//        if(user!=null){
+//            loggedUserDTO = new LoggedUserDTO();
+//            loggedUserDTO.setId(user.getId());
+//            loggedUserDTO.setLogin(user.getLogin());
+//            loggedUserDTO.setFirstName(user.getFirstName());
+//            loggedUserDTO.setLastName(user.getLastName());
+//        }
+//        return loggedUserDTO;
+//    }
+//
+//    public LoggedUserDTO getUser(String login) {
+//        return TestDataGenerator.getLoggedUserDTO(login);
+//    }
+//
+//    public SkillDTO buildSkillDTO(Skill skill) {
+//        SkillDTO skillDTO = new SkillDTO();
+//        skillDTO.setId(skill.getId());
+//        skillDTO.setName(skill.getName());
+//        skillDTO.setCategory(skill.getCategory());
+//
+//        return skillDTO;
+//    }
 }
--- src/main/java/com/wap/spring/data/repositories/UserRepository.java
+++ src/main/java/com/wap/spring/data/repositories/UserRepository.java
@@ -1,9 +1,28 @@
 package com.wap.spring.data.repositories;
-
+import com.wap.spring.data.model.Item;
 import com.wap.spring.data.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.hit.spring.data.model.Skill;
+import pl.hit.spring.data.model.User;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(nativeQuery = true,
+            value = "SELECT CASE WHEN count(*) >= 1 THEN 'true' ELSE 'false' END " +
+                    "FROM users WHERE login = ?1 AND password = ?2")
+    Boolean checkIfUserExists(String login, String password);
+
+    User getUserByLoginAndPassword(String login, String password);
+
+//    @Query(value = "SELECT s FROM User u JOIN u.ownedItems uks JOIN uks.connectedSkills s WHERE u.id =?1")
+//    List<Item> findAllNonDistinctObtainedSkillsForUser(Long id);
+//
+//    @Query(value = "SELECT s FROM User u JOIN u.knownSources k JOIN k.connectedSkills s")
+//    Set<Item> getAllSkillsForUser(Long id);
 }
