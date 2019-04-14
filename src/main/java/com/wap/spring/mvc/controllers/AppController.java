e5360bfe12b7784bcec3fbefcb34f155358aaf4c
Dominika Woźniak <dominika.dorota.wozniak@gmail.com>
2019-04-14 11:05:27 +0200

    edit

---
+++ src/main/java/com/wap/spring/mvc/controllers/AppController.java
@@ -0,0 +1,31 @@
+package com.wap.spring.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+public class AppController {
+
+    @GetMapping(path = "WardrobeIndex")
+    public String showStartPage() {
+        return "/js_data/WardrobeIndex";
+    }
+
+    @GetMapping(path = "/WardrobePodstrona")
+    public String showWardrobePodstrona(){
+        return "/js_data/WardrobePodstrona";
+
+    }
+    @GetMapping(path = "/podstrona")
+    public String showPodstrona(){
+        return "/js_data/podstrona";
+    }
+
+
+
+
+
+}
